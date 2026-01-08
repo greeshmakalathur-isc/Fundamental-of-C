54a9cbc15df42464c4491cb7f9c284645e653758
Greeshma kalattur <greeshmakalathur@gmail.com>
2026-01-08 13:47:14 +0530

    Add files via upload

---
+++ file14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("enter a year");
+    scanf("%d",&year);
+
+    if((year %4==0 && year %100 !=0) ||(year %400==0)){
+        printf("%d is a leapyear",year);
+
+    }
+    else {
+        printf("it is not a leap year");
+
+    }
+    return 0;
+}
